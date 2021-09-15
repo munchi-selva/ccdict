0cd124275d98b7d543b8d673ef6b420fc2665009
Margaret Chi <munchi@gmail.com>
2021-09-15 16:03:26 +1000

    Support dictionary lookup using multiple search terms

--- ccdict.py
+++ ccdict.py
@@ -36,6 +36,8 @@ DE_JYUTPING = "jyutping"
 DE_ENGLISH  = "english"
 DE_COMMENT  = "comment"
 
+DE_FIELDS   = [DE_TRAD, DE_SIMP, DE_PINYIN, DE_JYUTPING, DE_ENGLISH, DE_COMMENT]
+
 #
 # CC-CEDICT format:
 #   TRAD_CHIN SIMP_CHIN [PINYIN] /ENG 1/ENG 2/.../ENG N/
@@ -64,6 +66,29 @@ DICT_PATT       = "{}\s+{}\s+{}\s+{}?\s*{}?\s*{}?".format(TRAD_PATT,
                                                           COMMENT_PATT)
 
 ###############################################################################
+# A class to help with dictionary lookup
+###############################################################################
+class DictSearchTerm(object):
+    def __init__(self, search_value, search_field = DE_TRAD, use_re = False):
+        self.search_value = search_value
+        self.search_field = search_field
+        self.use_re = use_re
+
+    #
+    # A read-only property that specifies which search operation to use
+    #
+    @property
+    def search_op(self):
+        return "REGEXP" if self.use_re else "=" 
+
+    #
+    # A read-only property that specifies the SQL query condition
+    #
+    @property
+    def search_cond(self):
+        return "{} {} ?".format(self.search_field, self.search_op)
+
+###############################################################################
 # sqlite helper functions
 ###############################################################################
 
@@ -80,7 +105,7 @@ def regexp(pattern, field):
     :returns True if field matches pattern.
     """
     re_pattern = re.compile(pattern)
-    return re_pattern.match(field) is not None
+    return field and re_pattern.match(field) is not None
 ###############################################################################
 
 
@@ -144,23 +169,26 @@ def create_dict():
     sqlcon.execute(cedict_canto_orphans_query)
 ###############################################################################
 
-
 ###############################################################################
-def lookup_dict(search_term):
-    # type (str) -> None
+def lookup(search_exprs):
+    # type (List[DictSearchTerm]) -> None
     """
-    Looks up a term in the dictionary
+    Performs a dictionary lookup based on a combination of search terms
+
+    search_expr:    List of search terms
     """
 
+    where_clause = " AND ".join([search_expr.search_cond for search_expr in search_exprs]) 
+    where_values = tuple([search_expr.search_value for search_expr in search_exprs])
     #
     # Build a query that aggregates definitions for the search term
     # corresponding to the same Jyutping value
     #
     canto_query = "SELECT traditional AS traditional, jyutping AS jyutping, pinyin AS pinyin, group_concat(english, '/') AS defns\
                    FROM cc_canto \
-                   WHERE traditional REGEXP ? \
-                   GROUP BY jyutping, pinyin"
-    for row in sqlcon.execute(canto_query, (search_term,)):
+                   WHERE {} \
+                   GROUP BY jyutping, pinyin".format(where_clause)
+    for row in sqlcon.execute(canto_query, where_values):
         defns = row["defns"]
         if defns:
             defns = list(set(row["defns"].split("/")))
@@ -169,7 +197,22 @@ def lookup_dict(search_term):
         if defns:
             for defn in defns:
                 print("\t{}".format(defn))
+################################################################################
+
 ###############################################################################
+def lookup_dict(search_term, de_field = DE_TRAD, use_re = False):
+    # type (str, str, Bool) -> None
+    """
+    Looks up a term in the dictionary
+
+    search_term:    Search term
+    de_field:       The dictionary entry field to search
+    use_re:         If True, treats search_term as a regular expression
+    """
+
+    search_expr = DictSearchTerm(search_term, de_field, use_re)
+    lookup([search_expr])
+################################################################################
 
 
 ###############################################################################
@@ -253,4 +296,7 @@ def main():
 if __name__ == "__main__":
     main()
     create_dict()
-    lookup_dict("五.*")
+    lookup_dict("五")
+    jyut = DictSearchTerm("jyun.", DE_JYUTPING, True)
+    eng = DictSearchTerm("surname", DE_ENGLISH, True)
+    lookup([jyut, eng])
